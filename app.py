1d9db59e1ba69fd76022cc3b436bbbb9016db080
Shanto Roy <roy@Shantos-MacBook-Pro.local>
2021-05-13 10:21:10 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import cufflinks as cf
+import plotly
+import plotly.graph_objs as go
+import json
+import ast
+import os
+import time
+
+import sys
+sys.path.append('scripts/')
+
+from visualization import st_data_visualization
+
+
+def try_read_df(f, f_name):
+    filename, file_extension = os.path.splitext(f_name)
+    try:
+        if file_extension.startswith(".xls"):
+            return pd.read_excel(f)
+        elif file_extension.startswith(".csv"):
+            return pd.read_csv(f)
+        else:
+            st.write("File Type did not match")
+    except Exception as e:
+        st.write(e)
+
+
+def main():
+    # SideBar Settings
+    st.sidebar.title("Control Panel")
+    st.sidebar.info(
+            "Test"
+        )
+
+    # app functionalities
+    primary_function = st.sidebar.selectbox(
+        'Choose App Functionality', ["Upload CSV File", "Data Visualization", \
+                    "Data Cleanup", "Feature Selection", "Classification"])
+
+    if primary_function == "Upload CSV File":
+        uploaded_file = st.sidebar.file_uploader("Upload a CSV/Excel file", accept_multiple_files=False,\
+                                                                        type=("csv", "xls"))
+
+        if uploaded_file is not None:
+            data = try_read_df(uploaded_file, uploaded_file.name)
+            st.write("Here are the first ten rows of the File")
+            st.table(data.head(10))
+            file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,\
+                                                        "FileSize":uploaded_file.size}
+            st.sidebar.write(file_details)
+            with open(os.path.join("temp_data", "test.csv"), "wb") as f:
+                f.write(uploaded_file.getbuffer())
+
+    if primary_function == "Data Visualization":
+        st_data_visualization()
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/visualization.py
@@ -0,0 +1,173 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import cufflinks as cf
+
+
+class one_feature:
+    def __init__(self, df, x_col_name):
+        self.df = df 
+        self.x_col_name = x_col_name
+
+
+    def bar_plot(self):
+        #labels
+        key = self.df[self.x_col_name].value_counts().keys().tolist()
+        #values
+        val = self.df[self.x_col_name].value_counts().values.tolist()
+        trace = go.Bar(x = key, y=val,\
+                marker=dict(color=val,colorscale='Viridis',showscale=True),text = val)
+        data=[trace]
+        fig = go.Figure(data=data)
+        st.plotly_chart(fig)
+
+
+    def pi_plot(self):
+        #labels
+        key = self.df[self.x_col_name].value_counts().keys().tolist()
+        #values
+        val = self.df[self.x_col_name].value_counts().values.tolist()
+        trace = go.Pie(labels=key, 
+                values=val, 
+                marker=dict(colors=['red']), 
+                # Seting values to 
+                hoverinfo="value"
+              )
+        data = [trace]
+        fig = go.Figure(data = data)
+        st.plotly_chart(fig)
+
+
+    def histogram_plot(self):
+        # defining data
+        trace = go.Histogram(x=self.df[self.x_col_name],nbinsx=40,histnorm='percent')
+        data = [trace]
+        fig = go.Figure(data = data)
+        st.plotly_chart(fig)
+
+
+
+class two_features:
+    def __init__(self, df, x_col_name, y_col_name):
+        self.df = df 
+        self.x_col_name = x_col_name
+        self.y_col_name = y_col_name
+
+    def box_plot(self):
+        fig = px.box(self.df, x = self.x_col_name, y = self.y_col_name)
+        st.plotly_chart(fig)
+
+    def violin_plot(self):
+        fig = px.violin(self.df, x = self.x_col_name, y = self.y_col_name)
+        st.plotly_chart(fig)
+
+    def scatter_plot(self):
+        fig = px.scatter(self.df, x = self.x_col_name, y = self.y_col_name, color = self.y_col_name, \
+                            color_continuous_scale=px.colors.sequential.Viridis)
+        st.plotly_chart(fig)
+
+    def bar_plot(self):
+        self.df = self.df.groupby([self.x_col_name,self.y_col_name]).size().reset_index(name='quantity')
+        fig = px.bar(self.df,
+                    x = self.x_col_name,
+                    y = 'quantity',
+                    color = self.y_col_name,
+                    barmode = 'stack')
+        st.plotly_chart(fig)
+
+    
+
+
+    
+
+
+
+class three_features:
+    def __init__(self, df, x_col_name, y_col_name, category_col_name):
+        self.df = df 
+        self.x_col_name = x_col_name
+        self.y_col_name = y_col_name
+        self.category_col_name = category_col_name
+
+    def scatter_plot(self):
+        fig = px.scatter(self.df, x=self.x_col_name, y=self.y_col_name, \
+                            color=self.category_col_name)
+        st.plotly_chart(fig)
+
+    def line_plot(self):
+        fig = px.line(
+                data_frame=self.df,
+                x = self.x_col_name,
+                y = self.y_col_name,
+                color = self.category_col_name
+            )
+        st.plotly_chart(fig)
+
+
+
+
+def st_data_visualization():
+    # original saved database -> test.csv
+    # df = pd.read_csv("temp_data/test.csv")
+    # for code testing -> 5000_sales_records.csv
+    df = pd.read_csv("temp_data/5000_sales_records.csv")
+    column_list = df.columns.values.tolist()
+
+    target_feature_no = st.sidebar.selectbox(
+        'Choose No. of Target Features', ["One", "Two", "Three", "All"])
+    
+    if target_feature_no == 'One':
+        st.sidebar.write("Choose One Column")
+        x_col_name = st.sidebar.selectbox('Select X column', column_list)
+
+        plot_list = ["bar", "pi", "histogram"]
+        plot_type = st.sidebar.selectbox('Select Plot Type', plot_list)
+
+        plot = one_feature(df, x_col_name)
+        if plot_type == "bar":
+            plot.bar_plot()
+        if plot_type == "pi":
+            plot.pi_plot()
+        if plot_type == "histogram":
+            plot.histogram_plot()
+        
+
+    if target_feature_no == 'Two':    
+        st.sidebar.write("Choose Two Columns for Viewing Relationships")
+        x_col_name = st.sidebar.selectbox('Select X column', column_list)
+        y_col_name = st.sidebar.selectbox('Select Y column', column_list)
+
+        plot_list = ["box", "violin", "scatter", "bar"]
+        plot_type = st.sidebar.selectbox('Select Plot Type', plot_list)
+
+        plot = two_features(df, x_col_name, y_col_name)
+        if plot_type == "box":
+            plot.box_plot()
+        if plot_type == "violin":
+            plot.violin_plot()
+        if plot_type == "scatter":
+            plot.scatter_plot()
+        if plot_type == "bar":
+            plot.bar_plot()
+        
+
+
+
+    if target_feature_no == 'Three':    
+        st.sidebar.write("Choose Two Columns for Viewing Relationships")
+        x_col_name = st.sidebar.selectbox('Select X column', column_list)
+        y_col_name = st.sidebar.selectbox('Select Y column', column_list)
+
+        st.sidebar.write("Choose Category Column")
+        category_col_name = st.sidebar.selectbox('Select Category', column_list)
+
+        plot_list = ["scatter", "line"]
+        plot_type = st.sidebar.selectbox('Select Plot Type', plot_list)
+
+        plot = three_features(df, x_col_name, y_col_name, category_col_name)
+        if plot_type == "scatter":
+            plot.scatter_plot()
+        if plot_type == "line":
+            plot.line_plot()
